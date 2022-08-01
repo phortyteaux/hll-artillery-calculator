3ff483cf6af0261930660eb0d667faf2e4ce72ad
phortyteaux <phortyteaux@gmail.com>
2022-08-01 12:36:36 -0700

    finished version 1.0; program accepts command-line argument for picking your faction; accepts distance in meters and returns mils appropriate to given distance rounded up

--- hll_artillery_calculator.py
+++ hll_artillery_calculator.py
@@ -1,18 +1,38 @@
-us_german_table = {
-	1600 : 622,
-	1500 : 646,
-	1400 : 670,
-	1300 : 693,
-	1200 : 717,
-	1100 : 741,
-	1000 : 764,
-	900  : 788,
-	800  : 812,
-	700  : 836,
-	600  : 859,
-	500  : 883,
-	400  : 907,
-	300  : 930,
-	200  : 954,
-	100  : 978
-}
+"""********************************************************************************
+* Welcome to the HLL Artillery Calculator!                                        *
+* This program accepts a distance in meters and returns a value of mils to adjust *
+* your artillery barrel to. Run with 'de', 'us', or 'ru' to set the appropriate   *
+* calculation based on which faction you are playing.                             *
+********************************************************************************"""
+
+import sys
+
+def us_de_calculate(distance):
+	mils = (-0.237 * float(distance)) + 1002
+	return round(mils)
+
+def ru_calculate(distance):
+	mils = (-0.213 * float(distance)) + 1141
+	return round(mils)
+
+if sys.argv[1] != "us" and sys.argv[1] != "de" and sys.argv[1] != "ru":
+	print("Invalid command line option! Please re-run with 'us', 'de', or 'ru' as the only additional argument")
+	sys.exit(0)
+
+distance = 0
+print("Welcome to the HLL Artillery Calculator! Enter a distance in meters to get the appropriate amount of mils to adjust your gun to. Enter 'quit' to quit.")
+while distance != "quit":
+	distance = input("distance to target(m): ")
+
+	if sys.argv[1] == "us" or sys.argv[1] == "de":
+		try:
+			mils = us_de_calculate(distance)
+			print("mils to target:", mils)
+		except:
+			break
+	elif sys.argv[1] == "ru":
+		try:
+			mils = ru_calculate(distance)
+			print("mils to target:", mils)
+		except:
+			break
